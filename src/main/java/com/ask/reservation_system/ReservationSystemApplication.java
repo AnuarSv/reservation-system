c5a5a285184823572e1994b863f8dc2fe3c75467
AnuarSv <sultanbekov1706@gmail.com>
2026-04-21 10:17:04 +0500

    init

---
+++ src/main/java/com/ask/reservation_system/ReservationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.ask.reservation_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+// Основной класс для запуска, не трогается.
+@SpringBootApplication
+public class ReservationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservationSystemApplication.class, args);
+	}
+
+}
